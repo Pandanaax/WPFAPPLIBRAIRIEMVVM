d87e6be4216c64bae52edd7b164080bfe626d804
Randa <randa.bali@altran.com>
2020-12-18 15:18:08 +0100

    push

---
+++ WpfAppLibrairieMVVM/Helpers/Utils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppLibrairieMVVM.Helpers
+{
+    public class Utils
+    {
+        public static string[] GetListBook()
+        {
+            string _rep = ConfigurationManager.AppSettings["repfileExterne"];
+
+            var _listFic = Directory.GetFiles(_rep, "*.jpg", SearchOption.TopDirectoryOnly);
+
+            return _listFic;
+        }
+
+
+    }
+}
---
+++ WpfAppLibrairieMVVM/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfAppLibrairieMVVM.View;
+
+namespace WpfAppLibrairieMVVM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            frmPrincial.Navigate(new PagePrincipale());
+        }
+
+        private void GoToPageNouveauLivre_Click(object sender, RoutedEventArgs e)
+        {
+            frmPrincial.Navigate(new PageNouveauLivre());
+        }
+    }
+}
